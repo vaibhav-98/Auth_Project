4c8c31a06e0df13659e6be048e0ba0aab3617e00
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-07-10 21:09:13 +0530

    push code

--- controller/authController.js
+++ controller/authController.js
@@ -1,159 +1,142 @@
-const userModel = require('../models/userSchema')
-const emailValidator = require('email-validator')
-const bcrypt = require('bcrypt')
+const userModel = require("../models/userSchema");
+const emailValidator = require("email-validator");
+const bcrypt = require("bcrypt");
 
 //********************** singUp API****************************************************** */
-const signup = async (req,res,next) => {
-   const {name , email ,password, confirmPassword} = req.body;
-   console.log({name , email ,password, confirmPassword});
 
-   if(!name || !email || !password || !confirmPassword) {
-         return res.status(400).json({
-            success:false,
-            message: "Every field is required"
-         })
-   }
+const signup = async (req, res, next) => {
+  const { name, email, password, confirmPassword } = req.body;
+  console.log({ name, email, password, confirmPassword });
 
-   const validEmail = emailValidator.validate(email);
-    if (!validEmail) {
-
-        return res.status(400).json({
-            success:false,
-            message: "Please provide a valid email id"
-         })
-    }
-    if(password!==confirmPassword) {
-        return res.status(400).json({
-            success:false,
-            message: "passwor and confirmPassword doesn't match"
-         })
-    }
-
-   try {
-       const userInfo= userModel(req.body)
-       const result = await userInfo.save();
-
-
-       return res.status(200).json({
-        succuess :true ,
-        data : result
-       });
-       
-   } catch (error) {
-      if (error.code === 11000) {
-         return res.status(400).json({
-            success:false,
-            message:'Account Already exists with  provided email id '
-         })
-
-      }
+  if (!name || !email || !password || !confirmPassword) {
     return res.status(400).json({
-        success: false,
-        message : error.message
-    })
-    
-   }
-  
-}
+      success: false,
+      message: "Every field is required",
+    });
+  }
 
+  const validEmail = emailValidator.validate(email);
+  if (!validEmail) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide a valid email id",
+    });
+  }
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      success: false,
+      message: "passwor and confirmPassword doesn't match",
+    });
+  }
+
+  try {
+    const userInfo = userModel(req.body);
+    const result = await userInfo.save();
+
+    return res.status(200).json({
+      succuess: true,
+      data: result,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: "Account Already exists with  provided email id ",
+      });
+    }
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //****************************** SingIn API ************************************************ */
-const signin = async (req,res) => {
-    const {email, password} = req.body ;
 
-    if(!email || !password) {
-        return res.status(400).json({
-            success:false,
-            message: 'Every field is required'
-        })
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Every field is required",
+    });
+  }
+
+  try {
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(400).json({
+        success: false,
+        message: "invalid credentials",
+      });
     }
-       
 
-    try {
-
-        const user = await userModel.findOne({email}).select('+password');
-     
-    if(!user || !(await bcrypt.compare(password,user.password) )  ) {
-        return res.status(400).json({
-            success:false,
-            message: 'invalid credentials'
-        })
-    }
-      
     const token = user.jwtToken();
-    user.password = undefined ;
+    user.password = undefined;
 
-    const cookieOption ={
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly : true
+    const cookieOption = {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
     };
 
-    res.cookie("token" , token , cookieOption);
+    res.cookie("token", token, cookieOption);
     res.status(200).json({
-        success:true,
-        data: user
-    })
-        
-    } catch (error) {
-        res.status(400).json({
-            success:false,
-            message:error.message
-        })
-        
-    }
-    
-}
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-const getUser = async(req,res,next) =>{
-    const userId = req.user.id;
+//****************************getUser API******************************************* */
 
-    try {
-        const  user = await userModel.findById(userId)
-        return res.status(200).json({
-            success:true,
-            data: user
-        })
-        
-    } catch (error) {
-        return res.status(400).json({
-            success:false,
-            message: "error in controller"
-        })
-    }
+const getUser = async (req, res, next) => {
+  const userId = req.user.id;
 
-}
-
-
+  try {
+    const user = await userModel.findById(userId);
+    return res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "error in controller",
+    });
+  }
+};
 
 //**************************** logOut API***************************************** */
 
-const logout =(req,res) => {
+const logout = (req, res) => {
+  try {
+    const cookieOption = {
+      expires: new Date(),
+      httpOnly: true,
+    };
+    res.cookie("token", null, cookieOption);
+    res.status(200).json({
+      success: true,
+      message: "Logged out",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-    try {
-        const cookieOption = {
-            expires: new Date(),
-            httpOnly : true
-        };
-        res.cookie("token", null , cookieOption);
-        res.status(200).json({
-            success: true ,
-            message: "Logged out"
-        })
-    } catch (error) {
-        res.status(400).json({
-            success:false,
-            message: error.message
-        })
-        
-    }
-
-
-
-}
-
-module.exports= {
-    signup,
-    signin,
-    getUser,
-    logout,
-}
+module.exports = {
+  signup,
+  signin,
+  getUser,
+  logout,
+};
